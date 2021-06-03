c922fe27441e5f7c16bff3bb5ebae4ec5d460d49
Karim Assi <karim.assi@epfl.ch>
2021-06-03 18:32:13 +0200

    Style graph

--- website/app/js/network.js
+++ website/app/js/network.js
@@ -10,16 +10,28 @@ class Network {
 		self.container = container;
 		self.nodes = [];
 		self.edges = [];
+
+        sigma.settings.nodesPowRatio = 1;
+		sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+			var k,
+				neighbors = {},
+				index = this.allNeighborsIndex[nodeId] || {};
+		
+			for (k in index)
+			  neighbors[k] = this.nodesIndex[k];
+		
+			return neighbors;
+		  });
 	}
 
 	process_nodes(nodes) {
 		nodes.forEach(node => {
 			self.nodes.push({
 				id: node.id, 
-				// label: node.name,
+				label: node.name,
 				x: node.x,
 				y: node.y, 
-				size: 1,
+				size: 8,
 				color: node.color//getHexColor(2**node.audio)
 			})
 		});
@@ -32,7 +44,7 @@ class Network {
 				source: edge.source,
 				target: edge.target,
 				type: 'curve',
-				color: '#eee'
+				color: 'rgba(250,250,250,0.6)'
 			})
 		});
 	}
@@ -43,6 +55,7 @@ class Network {
 		let g = {nodes: self.nodes, edges: self.edges};
 
 		let s = new sigma({
+			autoRescale : false,
 			graph: g,
 			renderer: {
 				container: document.getElementById(self.container),
@@ -52,12 +65,71 @@ class Network {
 				doubleClickEnabled: false,
 				minEdgeSize: 0.5,
 				maxEdgeSize: 4,
-				enableEdgeHovering: true,
-				edgeHoverColor: 'edge',
-				defaultEdgeHoverColor: '#000',
-				edgeHoverSizeRatio: 1,
-				edgeHoverExtremities: true,
+				minNodeSize: 0,
+				maxNodeSize: 0,
+				drawLabels: false,
+				enableNodeHovering: false,
 			}
 		})
+
+		s.configNoverlap({
+			nodeMargin: 0.5,
+			scaleNodes: 0.85,
+			gridSize: 100,
+			easing: 'quadraticInOut', // animation transition function
+			duration: 10000   // animation duration. Long here for the purposes of this example only
+		  });
+
+		// Start the layout:
+		s.startNoverlap();
+
+
+		s.graph.nodes().forEach(function(n) {
+			n.originalColor = n.color;
+		  });
+		s.graph.edges().forEach(function(e) {
+			e.originalColor = e.color;
+		});
+
+		s.bind('clickNode', e => {
+			var nodeId = e.data.node.id;
+			var toKeep = s.graph.neighbors(nodeId);
+			toKeep[nodeId] = e.data.node;
+
+			s.graph.nodes().forEach(n => {
+				if (toKeep[n.id]) {
+					n.color = n.originalColor;
+				} else {
+					n.color = 'rgba(190,190,190,0.5)';
+				}
+			});
+
+			s.graph.edges().forEach(e => {
+				if (toKeep[e.source] && toKeep[e.target]) {
+					e.color = 'rgba(250,250,250,1)';
+				} else {
+					e.color = 'rgba(170,170,170,0.5)';
+				}
+			});
+
+			// Since the data has been modified, we need to
+			// call the refresh method to make the colors
+			// update effective.
+			s.refresh();
+		});
+
+		s.bind('clickStage', function(e) {
+			s.graph.nodes().forEach(function(n) {
+			  n.color = n.originalColor;
+			});
+	
+			s.graph.edges().forEach(function(e) {
+			  e.color = e.originalColor;
+			});
+	
+			// Same as in the previous event:
+			s.refresh();
+		});
+
 	}
 } 
